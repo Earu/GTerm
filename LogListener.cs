0ffc03cb19a7e1931b7630031f90c8862e14756a
Earu <earu.derg@gmail.com>
2021-12-26 17:59:47 +0100

    Add project files.

---
+++ LogListener.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.IO.Pipes;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+
+namespace GTerm
+{
+    internal class LogEventArgs : EventArgs
+    {
+        internal LogEventArgs(int type, int lvl, string grp, Color col, string msg)
+        {
+            this.Type = type;
+            this.Level = lvl;
+            this.Group = grp;
+            this.Color = col;
+            this.Message = msg;
+        }
+
+        public int Type { get; private set; }
+        public int Level { get; private set; }
+        public string Group { get; private set; }
+        public Color Color { get; private set; }
+        public string Message { get; private set; }
+
+    }
+
+    delegate void LogEventHandler(object sender, LogEventArgs args);
+
+    internal class LogListener
+    {
+        private const int BUFFER_SIZE = 8192;
+        private readonly byte[] Buffer = new byte[BUFFER_SIZE];
+
+        public event EventHandler OnConnected;
+        public event EventHandler OnDisconnected;
+        public event ErrorEventHandler OnError;
+        public event LogEventHandler OnLog;
+
+        public bool IsConnected { get; private set; }
+
+        private void SetConnectionStatus(bool connected)
+        {
+            if (connected)
+                this.OnConnected?.Invoke(this, EventArgs.Empty);
+            else
+                this.OnDisconnected?.Invoke(this, EventArgs.Empty);
+
+            this.IsConnected = connected;
+        }
+
+        private string ReadString(BinaryReader reader)
+        {
+            List<byte> list = new List<byte>();
+            byte ch = 0;
+            while ((ch = reader.ReadByte()) != 0)
+                list.Add(ch);
+
+            return Encoding.UTF8.GetString(list.ToArray());
+        }
+
+        private void ProcessMessages()
+        {
+            using (NamedPipeClientStream pipe = new NamedPipeClientStream(
+                    ".",
+                    "garrysmod_console",
+                    PipeAccessRights.Read | PipeAccessRights.WriteAttributes,
+                    PipeOptions.Asynchronous,
+                    TokenImpersonationLevel.Anonymous,
+                    HandleInheritability.None
+                ))
+            {
+                while (true)
+                {
+                    try
+                    {
+                        pipe.Connect();
+                        pipe.ReadMode = PipeTransmissionMode.Message;
+
+                        this.SetConnectionStatus(true);
+
+                        while (pipe.IsConnected)
+                        {
+                            try
+                            {
+                                int read = pipe.Read(this.Buffer, 0, BUFFER_SIZE);
+                                if (read == 0) continue;
+
+                                using (BinaryReader reader = new BinaryReader(new MemoryStream(this.Buffer, 0, read)))
+                                {
+                                    int type = reader.ReadInt32();
+                                    int level = reader.ReadInt32();
+                                    string group = this.ReadString(reader);
+                                    Color color = Color.FromArgb(
+                                        reader.ReadByte(),
+                                        reader.ReadByte(),
+                                        reader.ReadByte()
+                                    );
+
+                                    reader.ReadByte();
+
+                                    string msg = this.ReadString(reader);
+                                    this.OnLog?.Invoke(this, new LogEventArgs(type, level, group, color, msg.Replace("<NEWLINE>", "\n")));
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                this.OnError?.Invoke(this, new ErrorEventArgs(ex));
+                            }
+                        }
+
+                        this.SetConnectionStatus(false);
+                    }
+                    catch (TimeoutException) { } // ignore that
+                    catch (Exception ex)
+                    {
+                        this.OnError?.Invoke(this, new ErrorEventArgs(ex));
+                    }
+                }
+            }
+
+        }
+
+        public void Start()
+        {
+            Thread thread = new Thread(this.ProcessMessages);
+            thread.Start();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using Spectre.Console;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GTerm
+{
+    class Program
+    {
+        private static readonly object Locker = new object();
+        private static readonly LogListener Listener = new LogListener();
+        private static string ProcessPath = string.Empty;
+
+        static void Main()
+        {
+            Console.Title = "GTerm";
+
+            // prevent running it multiple times
+            Process curProc = Process.GetCurrentProcess();
+            string processName = curProc.ProcessName;
+            if (Process.GetProcesses().Count(p => p.ProcessName == processName) > 1)
+                return;
+
+            ProcessPath = Path.Combine(Path.GetDirectoryName(curProc.MainModule.FileName), "Archives");
+            if (!Directory.Exists(ProcessPath))
+                Directory.CreateDirectory("Archives");
+
+            ShowWaitingConnection();
+
+            Listener.OnConnected += (_, __) => AnsiConsole.Markup("[bold green]Connected![/]");
+            Listener.OnDisconnected += (_, __) =>
+            {
+                AnsiConsole.Markup("[bold red]Disconnected[/]");
+                ShowWaitingConnection();
+            };
+
+            Listener.OnLog += OnLog;
+            Listener.OnError += OnError;
+            Listener.Start();
+        }
+
+        private static void ShowWaitingConnection()
+        {
+            _ = AnsiConsole.Status()
+             .AutoRefresh(false)
+             .Spinner(Spinner.Known.Aesthetic)
+             .SpinnerStyle(Style.Parse("red bold"))
+             .StartAsync("Waiting for Garry's Mod connection...", async ctx =>
+             {
+                 while (true)
+                 {
+                     ctx.Refresh();
+                     await Task.Delay(500);
+                     if (Listener.IsConnected)
+                     {
+                         ctx.Status = "Connected";
+                         ctx.SpinnerStyle(Style.Parse("green cold"));
+                         ctx.Spinner(Spinner.Known.Star);
+                     }
+                 }
+             });
+        }
+
+        private static void OnError(object sender, ErrorEventArgs e)
+        {
+            string stackTrace = SanitizeLogMessage(e.GetException().ToString());
+            AnsiConsole.Markup($"[italic red]{stackTrace}[/]");
+        }
+
+        private static bool IsBlack(System.Drawing.Color col)
+            => col.R == 0 && col.G == 0 && col.B == 0;
+
+        private static string SanitizeLogMessage(string msg)
+        {
+            msg = Markup.Escape(msg);
+            return msg;
+        }
+
+        private static bool LastMsgHadNewLine = true;
+        private static void OnLog(object sender, LogEventArgs args)
+        {
+            lock (Locker)
+            {
+                //if (args.Message.Trim().Length == 0) return;
+                string log = args.Message;
+                string timeStamp = null; 
+
+                System.Drawing.Color col = IsBlack(args.Color) ? System.Drawing.Color.White : args.Color;
+                string mk = $"[rgb({col.R},{col.G},{col.B})]{SanitizeLogMessage(log)}[/]";
+
+                if (LastMsgHadNewLine)
+                {
+                    if (log == "\n") return;
+
+                    timeStamp = DateTime.Now.ToString("T");
+                    mk = $"[#ffaf00]{timeStamp}[/] | " + mk;
+                    log = $"{timeStamp} | {log}";
+                }
+
+                LastMsgHadNewLine = args.Message.EndsWith("\n");
+                AnsiConsole.Write(new Markup(mk));
+
+                string fileName = $"{DateTime.Now.ToString("d").Replace("/", "_")}.log";
+                File.AppendAllText(Path.Combine(ProcessPath, fileName), log);
+            }
+        }
+    }
+}
