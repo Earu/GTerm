cd1ad31519beb72734066029b66922251e840032
Earu <earu.derg@gmail.com>
2022-02-27 14:57:10 +0100

    Fix some edge cases, add a log file

---
+++ LocalLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace GTerm
+{
+    internal static class LocalLogger
+    {
+        internal static void WriteLine(params object[] args)
+        {
+            string gtermDir = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            string outMsg = $"{DateTime.Now:hh:mm:ss} | {string.Join("\t", args.Select(x => x.ToString()))}\n";
+
+            File.AppendAllText(Path.Combine(gtermDir, "gterm.log"), outMsg);
+        }
+    }
+}
