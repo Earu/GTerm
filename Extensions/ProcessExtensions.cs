25372f588d2ac5aa6a50cac548d857f391147ec7
Earu <ryan.rbsr@gmail.com>
2024-10-03 15:01:42 +0200

    make gterm buildable on linux/macos

--- Extensions/ProcessExtensions.cs
+++ Extensions/ProcessExtensions.cs
@@ -1,9 +1,14 @@
 ﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace GTerm.Extensions
 {
     public static class ProcessExtensions
     {
-        public static Process? GetParent(this Process process) => ParentProcessUtilities.GetParentProcess(process.Handle);
+        public static Process? GetParent(this Process process) {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return null;
+
+            return ParentProcessUtilities.GetParentProcess(process.Handle);
+        }
     }
 }   
