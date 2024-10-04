dac33ea45ba0ab014f2d6069486f5913e75f7413
Ryan <ryan.rbsr@gmail.com>
2024-10-04 02:03:42 +0200

    Account for <EOL> marker and partial data on the UNIX side

--- Listeners/UnixLogListener.cs
+++ Listeners/UnixLogListener.cs
@@ -6,8 +6,6 @@ namespace GTerm.Listeners
     internal class UnixLogListener : ILogListener
     {
         private const int BUFFER_SIZE = 8192;
-        private readonly byte[] Buffer = new byte[BUFFER_SIZE];
-
         private FileStream? Pipe;
 
         public event EventHandler? OnConnected;
@@ -37,9 +35,36 @@ namespace GTerm.Listeners
             return Encoding.UTF8.GetString(list.ToArray());
         }
 
+        private void ProcessMessage(byte[] buffer)
+        {
+            using (BinaryReader reader = new(new MemoryStream(buffer, 0, buffer.Length)))
+            {
+                int type = reader.ReadInt32();
+                int level = reader.ReadInt32();
+                string group = ReadString(reader);
+                Color color = Color.FromArgb(
+                    reader.ReadByte(),
+                    reader.ReadByte(),
+                    reader.ReadByte()
+                );
+
+                reader.ReadByte();
+
+                string fullMsg = ReadString(reader);
+                OnLog?.Invoke(this, new LogEventArgs(type, level, group, color, fullMsg));
+            }
+        }
+
 
         private async Task ProcessMessages()
         {
+            byte[] eolSequence = Encoding.UTF8.GetBytes("<EOL>");
+
+            int eolIndex = 0;
+            List<byte> dataBuffer = [];
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bytesRead;
+
             while (true)
             {
                 try
@@ -51,24 +76,37 @@ namespace GTerm.Listeners
                     {
                         try
                         {
-                            int read = await this.Pipe.ReadAsync(this.Buffer.AsMemory(0, BUFFER_SIZE));
-                            if (read == 0) continue;
-
-                            using (BinaryReader reader = new(new MemoryStream(this.Buffer, 0, read)))
+                            bytesRead = await this.Pipe.ReadAsync(buffer);
+                            if (bytesRead == 0)
                             {
-                                int type = reader.ReadInt32();
-                                int level = reader.ReadInt32();
-                                string group = ReadString(reader);
-                                Color color = Color.FromArgb(
-                                    reader.ReadByte(),
-                                    reader.ReadByte(),
-                                    reader.ReadByte()
-                                );
+                                await Task.Delay(50);
+                                continue;
+                            }
 
-                                reader.ReadByte();
+                            for (int i = 0; i < bytesRead; i++)
+                            {
+                                byte currentByte = buffer[i];
+                                dataBuffer.Add(currentByte);
 
-                                string fullMsg = ReadString(reader);
-                                OnLog?.Invoke(this, new LogEventArgs(type, level, group, color, fullMsg));
+                                // Check if the current byte matches the next byte in the EOL sequence
+                                if (currentByte == eolSequence[eolIndex])
+                                {
+                                    eolIndex++;
+                                    if (eolIndex == eolSequence.Length)  // Full EOL found
+                                    {
+                                        dataBuffer.RemoveRange(dataBuffer.Count - eolSequence.Length, eolSequence.Length);  // Remove <EOL> bytes
+
+                                        this.ProcessMessage([.. dataBuffer]);
+
+                                        dataBuffer.Clear();  // Clear the buffer for new data
+                                        eolIndex = 0;  // Reset the EOL matching index
+                                    }
+                                }
+                                else
+                                {
+                                    // If the byte doesn't match, reset the EOL matching index
+                                    eolIndex = 0;
+                                }
                             }
                         }
                         catch (Exception ex)
