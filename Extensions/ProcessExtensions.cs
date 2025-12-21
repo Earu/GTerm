e9976c3e7559dfae6ecf415f2269b2bb4cf0d3ca
Ryan <ryan.rbsr@gmail.com>
2025-12-21 18:42:38 +0100

    various fixes and improvements to full-proof further

--- Extensions/ProcessExtensions.cs
+++ Extensions/ProcessExtensions.cs
@@ -8,7 +8,14 @@ namespace GTerm.Extensions
         public static Process? GetParent(this Process process) {
             if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return null;
 
-            return ParentProcessUtilities.GetParentProcess(process.Handle);
+            try {
+                return ParentProcessUtilities.GetParentProcess(process.Handle);
+            }
+            catch (Exception ex)
+            {
+                LocalLogger.WriteLine($"Failed to get parent process: {ex.Message}");
+                return null;
+            }
         }
     }
-}   
+}
--- Extensions/Win32Extensions.cs
+++ Extensions/Win32Extensions.cs
@@ -4,52 +4,55 @@ using System.Runtime.InteropServices;
 
 namespace GTerm.Extensions
 {
-    internal class Win32Extensions
+    internal partial class Win32Extensions
     {
         internal const int SW_MINIMIZE = 6;
         internal const int SW_HIDE = 0;
 
         internal delegate bool ConsoleEventDelegate(int eventType);
 
-        [DllImport("Kernel32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
-        internal static extern IntPtr GetConsoleWindow();
+        [LibraryImport("Kernel32.dll", SetLastError = true)]
+        [UnmanagedCallConv(CallConvs = new[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
+        internal static partial IntPtr GetConsoleWindow();
 
-        [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        [LibraryImport("User32.dll", SetLastError = true)]
+        [UnmanagedCallConv(CallConvs = new[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
         [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool ShowWindow([In] IntPtr hWnd, [In] int nCmdShow);
+        internal static partial bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-        [DllImport("User32.dll", CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        [LibraryImport("User32.dll", SetLastError = true)]
+        [UnmanagedCallConv(CallConvs = new[] { typeof(System.Runtime.CompilerServices.CallConvStdcall) })]
         [return: MarshalAs(UnmanagedType.Bool)]
-        internal static extern bool IsWindowVisible([In] IntPtr hWnd);
+        internal static partial bool IsWindowVisible(IntPtr hWnd);
 
-        [DllImport("kernel32.dll", SetLastError = true)]
-        internal static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
+        [LibraryImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static partial bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, [MarshalAs(UnmanagedType.Bool)] bool add);
 
         internal enum WinMessages : uint
         {
             /// <summary>
-            /// An application sends the WM_SETICON message to associate a new large or small icon with a window. 
-            /// The system displays the large icon in the ALT+TAB dialog box, and the small icon in the window caption. 
+            /// An application sends the WM_SETICON message to associate a new large or small icon with a window.
+            /// The system displays the large icon in the ALT+TAB dialog box, and the small icon in the window caption.
             /// </summary>
             SETICON = 0x0080,
         }
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, IntPtr lParam);
+        [LibraryImport("user32.dll", EntryPoint = "SendMessageW")]
+        private static partial IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, IntPtr lParam);
 
+        [System.Runtime.Versioning.SupportedOSPlatform("windows6.1")]
         private static void SetWindowIcon(System.Drawing.Icon icon)
         {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return;
-
-            IntPtr mwHandle = System.Diagnostics.Process.GetCurrentProcess().MainWindowHandle;
+            IntPtr mwHandle = Process.GetCurrentProcess().MainWindowHandle;
             SendMessage(mwHandle, (int)WinMessages.SETICON, 0, icon.Handle);
             SendMessage(mwHandle, (int)WinMessages.SETICON, 1, icon.Handle);
         }
 
         internal static void SetConsoleIcon(string iconFilePath)
         {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) return;
-            
+            if (!OperatingSystem.IsWindowsVersionAtLeast(6, 1)) return;
+
             if (!string.IsNullOrEmpty(iconFilePath))
             {
                 System.Drawing.Icon? icon = System.Drawing.Icon.ExtractAssociatedIcon(iconFilePath);
@@ -65,7 +68,7 @@ namespace GTerm.Extensions
     /// A utility class to determine a process parent.
     /// </summary>
     [StructLayout(LayoutKind.Sequential)]
-    internal struct ParentProcessUtilities
+    internal partial struct ParentProcessUtilities
     {
         // These members must match PROCESS_BASIC_INFORMATION
         internal IntPtr Reserved1;
@@ -75,8 +78,8 @@ namespace GTerm.Extensions
         internal IntPtr UniqueProcessId;
         internal IntPtr InheritedFromUniqueProcessId;
 
-        [DllImport("ntdll.dll")]
-        private static extern int NtQueryInformationProcess(IntPtr processHandle, int processInformationClass, ref ParentProcessUtilities processInformation, int processInformationLength, out int returnLength);
+        [LibraryImport("ntdll.dll")]
+        private static partial int NtQueryInformationProcess(IntPtr processHandle, int processInformationClass, ref ParentProcessUtilities processInformation, int processInformationLength, out int returnLength);
 
         /// <summary>
         /// Gets the parent process of a specified process.
@@ -85,8 +88,8 @@ namespace GTerm.Extensions
         /// <returns>An instance of the Process class.</returns>
         public static Process? GetParentProcess(IntPtr handle)
         {
-            ParentProcessUtilities pbi = new ParentProcessUtilities();
-            int status = NtQueryInformationProcess(handle, 0, ref pbi, Marshal.SizeOf(pbi), out int returnLength);
+            ParentProcessUtilities pbi = new();
+            int status = NtQueryInformationProcess(handle, 0, ref pbi, Marshal.SizeOf(pbi), out int _);
             if (status != 0)
                 throw new Win32Exception(status);
 
